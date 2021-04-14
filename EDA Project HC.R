1bb4de4c5a886386bac12ba0c58a748cb86a543f
Chris Merkord <chrismerkord@gmail.com>
2021-04-13 21:55:40 -0500

    Clean up script, run filter, add output files

--- EDA Project HC.R
+++ EDA Project HC.R
@@ -4,37 +4,52 @@
 library(tidyverse)
 library(auk)
 
+
+# filter ebird data -------------------------------------------------------
+
+# check if data folder exists, if not then create it
 if(!dir.exists("Data")) dir.create("Data")
 
-citation()
-
-R.Version()
-
-RStudio.Version()
-
-library(auk)
 # path to the ebird data file
 f_ebd_in <- "Data/ebd_merlin_relFeb-2021/ebd_merlin_relFeb-2021.txt"
 f_smp_in <- "Data/ebd_sampling_relFeb-2021/ebd_sampling_relFeb-2021.txt"
+
 # output text file
 f_ebd_out <- "Data/ebd_filtered_merlin.txt"
 f_smp_out <- "Data/edaproj_filtered_smp.txt"
-ebird_data <- f_ebd_in %>% 
-  # 1. reference file
-  auk_ebd(file_sampling = f_smp_in) %>% 
-  # 2. define filters
+f_awk <- "Data/merlin_awk_file.txt"
+
+# create a toggle switch for executing the filter
+# that way the next code chunk runs but skips the output if it already exists
+execute_filter <- !all(file.exists(c(f_ebd_out, f_smp_out)))
+
+# create reference to ebird data file and add filters
+auk_ebd(
+  file = f_ebd_in, 
+  file_sampling = f_smp_in
+) %>% 
   auk_bcr( bcr = 11) %>% 
   auk_date(date = c("*-05-01", "*-06-30")) %>% 
   auk_time(start_time = c("05:00", "19:00")) %>%
   auk_distance(distance = c(0,4)) %>%
-  auk_complete() %>%
-  # 3. run filtering
-  auk_filter(file = f_ebd_out, file_sampling = f_smp_out) %>% 
-  # 4. read text file into r data frame
-  read_ebd()
+  auk_complete() %>% 
+  auk_filter(                      # filter the eBird file using AWK
+    file = f_ebd_out,              # add an ebird data output path
+    file_sampling = f_smp_out,     # add a sampling data output path
+    awk_file = f_awk,              # this is required if execute = FALSE
+    execute = execute_filter       # if  output already exists, don't execute
+  )
 
 
+# read filtered data ------------------------------------------------------
+
+# read files
+ebd <- read_ebd("Data/ebd_filtered_merlin.txt")
+smp <- read_sampling("Data/edaproj_filtered_smp.txt")
 
 
+# citations ---------------------------------------------------------------
 
-
+citation()
+R.Version()
+RStudio.Version()
